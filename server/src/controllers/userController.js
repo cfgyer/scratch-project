85a890367241ff29947fa832a5e82c9342518dd6
Charles Gyer <charlesgyer@gmail.com>
2021-09-28 19:59:28 -0700

    user controller built out, product controller partial

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -6,70 +6,87 @@ const userController = {};
 userController.createUser = async (req, res, next) => {
 
   const {email, secret_password} = req.body;
-  console.log(email, secret_password)
-  // const currentUserName = req.body.username;
-  // const currentUserPassword = req.body.password;
-  // console.log(currentUserName, currentUserPassword);
-  // write a sql query string that creates a record in the users table corresponding to username and password
-    // TODO: find out if elephant automatically gives you unique user_id
-  // async await query result from db
-  // wrap above in try catch
-  // return next()
-  // const col1 = 'username';
-  // const col2 = 'password'
-
-  const sqlStr = "INSERT INTO users(username, password) VALUES (?, ?);";
-
-  const sqlStr2 = `INSERT INTO users(username) Values ('nonsense');`
-  const sqlStr3 = "INSERT INTO users(username, password) VALUES($1, $2);"
+  const sqlStr = "INSERT INTO users(username, password) VALUES($1, $2);"
   const values = [email, secret_password]
   try {
-  await db.query(sqlStr3, values);
-    console.log('successfully hit database');
+  await db.query(sqlStr, values);
+    console.log('successfully inserted user into database');
     return next();
   } catch(error) {
     return next(error);
   }
                   
   // in elephant sql, you need a "prepared" statement query string to avoid sql injections; php explanation here: https://stackoverflow.com/questions/4712037/what-is-parameterized-query
-  // how to keep track of how many user ids have been used up to know what is next
-  
   // form => posts to req.body
     // <input name="username" value=""/>
     // <input name="password" value={e.target.value}/>
+}
 
-  // pull the username and password off the request body 
-    // - because the username and password came from an input form, they will be key value pairs on req.body - the keys will match the name attributes on the form element; and the key value will match what the client typed into form
-    // urlstring/:param?qs1=query1
-
-
-  // const firstname = req.query.firstname;
-  // const lastname = req.query.lastname;
-  // const email = req.query.email;
-
+userController.getOneUser = async (req, res, next) => {
+  const sqlStr = 'SELECT * FROM users WHERE username= $1';
+  // discuss with Christian whether he wants this coming of the query string, or request body
+  const value = [req.body.email];
+  try {
+    // TODO: handle non-error to db, but no user record matches
+    const result = await db.query(sqlStr, value);
+    console.log('successfully retrieved one user from database');
+    const output = result.rows[0];
+    return res.send(output);
+  } catch(err) {
+    return next(err);
+  }
 }
 
 userController.getAllUsers = async (req, res, next) => {
-
   const sqlStr = 'SELECT * FROM users';
   try {
     const result = await db.query(sqlStr);
+    console.log('successfully retrieved all users from database');
     const output = result.rows;
     return res.send(output);
   } catch(err) {
     return next(err);
   }
-
 }
 
-userController.getOneUser = async (req, res, next) => {
-
+// TODO: make sure the update function only updates one user - right now, will update all who have username and password
+// user will not know what their unique id is
+userController.updateUser = async (req, res, next) => {
+  const sqlStr = 'UPDATE users SET username=$1, password=$2 WHERE username=$3 AND password=$4';
+  const values = [req.body.new_email, req.body.new_password, req.body.email, req.body.secret_password];
+  try {
+    const output = await db.query(sqlStr, values);
+    console.log(output)
+    if (output.rowCount > 0) {
+      console.log('successfully updated one user');
+      res.locals.username = req.body.new_email;
+    } else {
+      return res.json('could not find a user with that username and password');
+    }
+    
+    return next();
+  } catch(err) {
+    console.log('error connecting to database')
+    return next(err);
+  }
 }
 
-userController.updateUser = async (req, res, next) => {}
-
-userController.deleteUser = async (req, res, next) => {}
-
+userController.deleteUser = async (req, res, next) => {
+  const sqlStr = 'DELETE FROM users WHERE username=$1 AND password=$2';
+  const values = [req.body.email, req.body.secret_password];
+  try {
+    const result = await db.query(sqlStr, values);
+    if (result.rowCount > 0) {
+      console.log('successfully delete user');
+    } else {
+      return res.json('could not find a user with that username and password');
+    }
+    return next();
+  } catch(err) {
+    console.log('error connecting to database');
+    return next(err);
+  }
+}
 
 
 module.exports = userController;
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,28 +1,28 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
 
-const router = Router()
+const router = Router();
 
+
+// TODO: currently working on assumption that all requests in middleware 
 router.post("/", userController.createUser, (req, res) => {
-  // currently result is stored on res.locals.userinfo - discuss with Christian
   return res.send(`Created user: ${req.body.email}`);
 });
 
+router.get('/:user_id', userController.getOneUser, (req, res) => {
+  return res.send(`Hello from server/${req.params.user_id}`);
+});
+
 router.get('/', userController.getAllUsers, (req, res) => {
-  return res.send('<h1>Hello from server/user</h1>')
+  return res.send('<h1>Hello from server/user</h1>');
 });
 
-// decide whether this needs to be user id or username
-router.get('/:user_id', (req, res) => {
-  return res.send(`Hello from server/${req.params.user_id}`)
+router.patch("/", userController.updateUser, (req, res) => {
+  return res.send(`Updated user ${req.body.email} to ${res.locals.username}`);
 });
 
-router.patch("/", (req, res) => {
-  return res.send(`Updated user ${req.body.user_id} to ${req.body.updatedProduct.user_id}`)
-});
-
-router.delete('/:user_id', (req, res) => {
-  return res.send(`Deleted product ${req.params.user_id}`)
+router.delete('/:user_id', userController.deleteUser, (req, res) => {
+  return res.send(`Deleted user ${req.params.user_id}`);
 });
 
 
