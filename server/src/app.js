58b7f860fc54d16f495c15a006a89ab384dabe3d
Charles Gyer <charlesgyer@gmail.com>
2021-10-01 17:27:05 -0700

    working passport strat

--- server/src/app.js
+++ server/src/app.js
@@ -32,37 +32,52 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session())
 
-// passport.serializeUser((user,done) => {
-//     //user.id prop name?
-//     done(null, user.id)
-// })
+passport.serializeUser((user,done) => {
+    //user.id prop name?
+    done(null, user.username)
+})
 
-// passport.deserializeUser((idebugger, done) => {
-//     //db user lookup
-//     // if err done(err)
-//     //done(null, user)
-// })
+passport.deserializeUser( async (username, done) => {
+    //db user lookup
+    const sqlStr = 'SELECT * FROM users WHERE username=$1';
+    const value = [username];
+    const user = await db.query(sqlStr, value);
+    return done(null, user.rows[0]);
+
+    // db.users.findById(id, function(err, user) {
+    //   if(err) return done(err)
+    //   done(null, user);
+    // });
+})
 
 passport.use(
-    new LocalStrategy( async function(username, password, cb) {
+    new LocalStrategy( async function(username, password, done) {
         // Check if user exists in our db
         const sqlStr = 'SELECT * FROM users WHERE username=$1';
         const value = [username];
-        const userQueryResult = db.query(sqlStr, value); 
-        // If user found
-        if (userQueryResult.rows > 0) {
-          // Check plaintext password against db password
-          const hashCheck = await bcrypt.compare(password, userQueryResult.rows[0].password);
-          // If user exists and our database 
-          if (hashCheck) {
-            console.log('hash check worked \n', userQueryResult)
-            return cb(null, userQueryResult);
-          } else {
-            return cb(null, false);
-          }
-        } else {
-          return cb(null, false);
-        }
+        const userQueryResult = await db.query(sqlStr, value); 
+        console.log("User query result: \n", userQueryResult);
+        // if no user
+        if (userQueryResult.rows <= 0 ) return done(null, false)
+        // check if password matches db
+        const hashCheck = await bcrypt.compare(password, userQueryResult.rows[0].password);
+        if (!hashCheck) return done(null, false)
+        // Success: user found in db & plaintext password matches db password
+        return done(null, userQueryResult.rows[0])
+
+        // // If user found
+        // if (userQueryResult.rows > 0) {
+        //   // Check plaintext password against db password
+        //   // If user exists and our database 
+        //   if (hashCheck) {
+        //     console.log('hash check worked \n', userQueryResult)
+        //     return cb(null, userQueryResult.rows[0]);
+        //   } else {
+        //     return cb(null, false);
+        //   }
+        // } else {
+        //   return cb(null, false);
+        // }
     })
 )
 
@@ -73,7 +88,12 @@ app.use((req, res, next) => {
 })
 
 app.post("/auth/login", passport.authenticate('local'), (req, res) => {
-    console.log(req.user)
+    console.log("This is your user: \n", req.user);
+    console.log("This is your session \n", req.session);
+    console.log("Are you auth'd? \n", req.isAuthenticated())
+    req.logout();
+    console.log("Are you auth'd? \n", req.isAuthenticated())
+    res.send("Hey idiot you logged in.")
   });
   
 
